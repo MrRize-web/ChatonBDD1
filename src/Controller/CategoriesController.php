55f8762550cf7e19cf9035299829fc4809919761
MrRize-web <54984438+MrRize-web@users.noreply.github.com>
2019-11-14 09:32:49 +0100

    Préparation

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Categorie;
+use App\Form\CategorieType;
+
+class CategoriesController extends AbstractController
+{
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function index()
+    {
+        return $this->render('categories/index.html.twig', [
+            'controller_name' => 'CategoriesController',
+        ]);
+    }
+
+    public function ajouter()
+    {
+        //je crée un objet catégorie vide
+        $categorie=new Categorie();
+
+        //créer le formulaire
+        $formulaire=$this->createForm(CategorieType::Class, $categorie);//créer un formulaire vide
+        return $this->render('categories/formulaire.html.twig',[
+            "formulaire"=>$formulaire->createView()
+            ,"h1"=>"Ajouter une catégorie"
+        ])
+    }
+}
